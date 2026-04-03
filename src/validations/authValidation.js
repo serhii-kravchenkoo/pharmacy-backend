409af56e60bf42ff98fdb74f623852074db6d36a
Serhii Kravcnenko <sergkrava1983@gmail.com>
2026-04-03 23:59:27 +0300

    loginUserSchema

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -4,5 +4,18 @@ export const registerUserSchema = {
   [Segments.BODY]: Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(8).required(),
+    name: Joi.string().min(3).max(30).required(),
+    phone: Joi.string()
+      .min(10)
+      .max(15)
+      .required()
+      .pattern(/^\+?\d{10,15}$/),
+  }),
+};
+
+export const loginUserSchema = {
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
   }),
 };
