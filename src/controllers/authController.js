44a69e7ed001af145b6ec0915aef4c176fee4832
Serhii Kravcnenko <sergkrava1983@gmail.com>
2026-04-08 23:38:24 +0300

    logoutUser

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -48,3 +48,14 @@ export const loginUser = async (req, res) => {
 
   res.status(200).json(user);
 };
+
+export const logoutUser = async (req, res) => {
+  const { sessionId } = req.cookies;
+  if (sessionId) {
+    await Session.deleteOne({ _id: sessionId });
+  }
+  res.clearCookie('sessionId');
+  res.clearCookie('accessToken');
+  res.clearCookie('refreshToken');
+  res.status(204).send();
+};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,11 +3,16 @@ import {
   loginUserSchema,
   registerUserSchema,
 } from '../validations/authValidation.js';
-import { loginUser, registerUser } from '../controllers/authController.js';
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from '../controllers/authController.js';
 import { Router } from 'express';
 
 const router = Router();
 
 router.post('/api/user/register', celebrate(registerUserSchema), registerUser);
 router.post('/api/user/login', celebrate(loginUserSchema), loginUser);
+router.post('/api/user/logout', logoutUser);
 export default router;
