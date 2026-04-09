8f17c0b9d3f1a7918a9f679f8e13343219bc8a93
Serhii Kravcnenko <sergkrava1983@gmail.com>
2026-04-09 22:41:36 +0300

    refreshUserSession

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,6 +6,7 @@ import {
 import {
   loginUser,
   logoutUser,
+  refreshUserSession,
   registerUser,
 } from '../controllers/authController.js';
 import { Router } from 'express';
@@ -15,4 +16,5 @@ const router = Router();
 router.post('/api/user/register', celebrate(registerUserSchema), registerUser);
 router.post('/api/user/login', celebrate(loginUserSchema), loginUser);
 router.post('/api/user/logout', logoutUser);
+router.post('/api/user/refresh', refreshUserSession);
 export default router;
