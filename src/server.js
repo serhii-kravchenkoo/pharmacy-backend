7671a4f0f897382dc91e8218f8b8a18e4f7b8d27
Serhii Kravcnenko <sergkrava1983@gmail.com>
2026-05-06 22:55:52 +0300

    fix cors

--- src/server.js
+++ src/server.js
@@ -19,7 +19,12 @@ const PORT = process.env.PORT ?? 3000;
 // глобальні middleware
 app.use(logger);
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 
 // підключаємо групу маршрутів
